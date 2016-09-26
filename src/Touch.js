edee174e0f2fa31e7157847ac4259e79fd96b774
Carlos Crasborn <c.crasborn@gmail.com>
2016-09-26 13:09:05 +1300

    Fix delayed touch not being canceled

--- src/Touch.js
+++ src/Touch.js
@@ -302,6 +302,8 @@ export class TouchBackend {
     }
 
     handleTopMoveEndCapture (e) {
+        clearTimeout(this.timeout);
+
         if (!this.monitor.isDragging() || this.monitor.didDrop()) {
             this.moveStartSourceIds = null;
             return;
